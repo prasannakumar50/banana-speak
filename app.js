b30b2fce2f7fc24be5f46ba465359321d896e641
prasannakumar50 <babblu293@gmail.com>
2023-08-21 14:19:57 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var btnTranslate=document.querySelector("#btn-translate");
-
 var txtInput=document.querySelector("#txt-input");
-console.log(txtInput);
+var outputDiv = document.querySelector("#output");
+
+console.log(outputDiv.innerHTML);
 
 function clickEventHandler(){
-    console.log("clicked!");
 
-    console.log("input", txtInput.value);
+    outputDiv.innerHTML="jhjvhhgv " + txtInput.value; 
  }
 
 
