f00396802ff9e847f4bdea36a3a033209c74267d
prasannakumar50 <babblu293@gmail.com>
2023-08-22 12:19:02 +0530

    done

--- app.js
+++ app.js
@@ -2,11 +2,33 @@ var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-console.log(outputDiv.innerHTML);
+var serverURL ="https://api.funtranslations.com/translate/minion.json";
+
+function getTraslatedURL(text)
+{
+    return serverURL + "?" + "text= "  + text 
+}
+
+
+function errorHandler(error){
+    console.log("error occured", error);
+}
+
+
 
 function clickEventHandler(){
 
-    outputDiv.innerHTML="jhjvhhgv " + txtInput.value; 
+    var inputText= txtInput.value; 
+
+    //calling server for processing
+      fetch(getTraslatedURL(inputText))
+      .then(response => response.json())
+      .then(json=>{ 
+          var translatedText=json.contents.translated;
+          outputDiv.innerHTML=translatedText;
+        
+        })
+      .catch(errorHandler)
  }
 
 
